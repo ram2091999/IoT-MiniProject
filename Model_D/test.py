9ec1885042c4b2412882d003b63aca45df715e50
Sai Mitheran <49593646+smj007@users.noreply.github.com>
2021-04-27 15:44:02 +0530

    added new models

---
+++ Model_D/test.py
@@ -0,0 +1,71 @@
+#Imports   
+
+import train
+import sys
+from glob import iglob
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from keras.models import load_model
+from sklearn.model_selection import train_test_split
+import lime
+import lime.lime_tabular
+
+
+# Test script
+def test(num_features, model_name):
+    df = train.load_data()
+    test_with_data(num_features, model_name, df)
+
+def test_with_data(num_features, model_name, df):
+    X = df.drop(columns=['class'])
+    if num_features is not None:
+        fisher = pd.read_csv('../fisher.csv')
+        features = fisher.iloc[0:int(num_features)]['Feature'].values
+        X = X[list(features)]
+    Y = pd.get_dummies(df['class'])
+    classes = Y.columns.tolist()
+    print('Splitting data')
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    print('Transforming data')
+    scaler.fit(x_train)
+    x_test_scaled = scaler.transform(x_test)
+
+    model = load_model(f'models/{model_name}')
+    wrapper = ModelWrapper(model, scaler)
+    print('Model evaluation')
+    print('Loss, accuracy')
+    print(model.evaluate(x_test_scaled, y_test))
+    y_pred_proba = model.predict(x_test_scaled)
+    y_pred = np.argmax(y_pred_proba, axis=1)
+
+    cnf_matrix = confusion_matrix(np.argmax(y_test.values, axis=1), y_pred)
+    print('Confusion matrix')
+    print(classes)
+
+    print(cnf_matrix)
+
+    print('Explaining data using lime')
+    explainer = lime.lime_tabular.LimeTabularExplainer(x_train.values, feature_names=X.columns.tolist(), class_names=classes, discretize_continuous=True)
+    for j in range(10):
+        i = np.random.randint(0, x_test.shape[0])
+        print(f'Explaining for record nr {i}')
+        exp = explainer.explain_instance(x_test.values[i], wrapper.scale_predict, num_features=int(num_features), top_labels=5)
+        exp.save_to_file(f'lime/explanation{j}.html')
+        print(y_test.values[i])
+        print(exp.as_list())
+
+class ModelWrapper:
+    def __init__(self, model, scaler):
+        self.model = model
+        self.scaler = scaler
+
+    def scale_predict(self, x):
+        x = self.scaler.transform(x)
+        return self.model.predict(x)
+
+if __name__ == '__main__':
+    test(*sys.argv[1:])
---
+++ Model_D/train.py
@@ -0,0 +1,106 @@
+
+# Python Imports
+
+import sys
+import os
+from glob import iglob
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import pickle
+
+# Scikit-learn imports 
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+# Keras imports 
+from keras.models import model_from_yaml
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Activation
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.optimizers import Adam
+
+
+
+def load_data():
+    print('Loading data')
+    print('Loading gafgyt data')
+    df_gafgyt = pd.concat((pd.read_csv(f) for f in iglob('/content/botnet-traffic-analysis/data/**/gafgyt_attacks/*.csv', recursive=True)), ignore_index=True)
+    print('Loaded, shape: ')
+    print(df_gafgyt.shape)
+    df_gafgyt['class'] = 'gafgyt'
+    print('Loading mirai data')
+    df_mirai = pd.concat((pd.read_csv(f) for f in iglob('/content/botnet-traffic-analysis/data/**/mirai_attacks/*.csv', recursive=True)), ignore_index=True)
+    print('Loaded, shape: ')
+    print(df_mirai.shape)
+    df_mirai['class'] = 'mirai'
+    print('Loading benign data')
+    df_benign = pd.concat((pd.read_csv(f) for f in iglob('/content/botnet-traffic-analysis/data/**/benign_traffic.csv', recursive=True)), ignore_index=True)
+    print('Loaded, shape: ')
+    print(df_benign.shape)
+    df_benign['class'] = 'benign'
+    df = df_benign.append(df_gafgyt.sample(n=df_benign.shape[0], random_state=17)).append(df_mirai.sample(n=df_benign.shape[0], random_state=17))
+    return df
+
+
+def create_model(input_dim, add_hidden_layers, hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(hidden_layer_size, activation="tanh", input_shape=(input_dim,)))
+    for i in range(add_hidden_layers):
+        model.add(Dense(hidden_layer_size, activation="tanh"))
+    model.add(Dense(3))
+    model.add(Activation('softmax'))
+    return model
+
+def train(top_n_features = None):
+    df = load_data()
+    train_with_data(top_n_features, df)
+
+def train_with_data(top_n_features = None, df = None):
+    X = df.drop(columns=['class'])
+    if top_n_features is not None:
+        fisher = pd.read_csv('/content/botnet-traffic-analysis/data/top_features_fisherscore.csv')
+        features = fisher.iloc[0:int(top_n_features)]['Feature'].values
+        X = X[list(features)]
+    Y = pd.get_dummies(df['class'])
+    print('Splitting data')
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    print('Transforming data')
+    scaler.fit(x_train)
+    input_dim = X.shape[1]
+    scalerfile = f'./models/scaler_{input_dim}.sav'
+    pickle.dump(scaler, open(scalerfile, 'wb'))
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+    print('Creating a model')
+    
+    model = create_model(input_dim, 1, 128)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    cp = ModelCheckpoint(filepath=f'./models/model_{input_dim}.h5',
+                               save_best_only=True,
+                               verbose=0)
+    tb = TensorBoard(log_dir=f'./logs',
+                histogram_freq=0,
+                write_graph=True,
+                write_images=True)
+    epochs = 25
+    model.fit(x_train, y_train,
+                    epochs=epochs,
+                    batch_size=256,
+                    validation_data=(x_test, y_test),
+                    verbose=1,
+                    callbacks=[tb, cp])
+    print('Model evaluation')
+    print('Loss, Accuracy')
+    print(model.evaluate(x_test, y_test))
+    y_pred_proba = model.predict(x_test)
+    y_pred = np.argmax(y_pred_proba, axis=1)
+    cnf_matrix = confusion_matrix(np.argmax(y_test.values, axis=1), y_pred)
+    print('Confusion matrix')
+    print('benign  gafgyt  mirai')
+    print(cnf_matrix)
+
+if __name__ == '__main__':
+    train(*sys.argv[1:])
